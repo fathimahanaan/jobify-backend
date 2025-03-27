f58c593eed50dfe3afa054462157f3ceb7694962
fathimahanaan <hanaanfh764@icloud.com>
2025-03-27 13:55:43 +0530

    creating api

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import { nanoid } from "nanoid";
 
-fetch("https://www.course-api.com/react-useReducer-cart-project")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://www.course-api.com/react-useReducer-cart-project")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data)); this code used for fetching data or u can use next line of code
+
+// try {
+//   const response = await fetch(
+//     "https://www.course-api.com/react-useReducer-cart-project"
+//   );
+//   const cartData = await response.json();
+//   console.log(cartData);
+// } catch (error) {
+//   console.log(error);
+// }
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "googl", position: "backend-end" },
+  { id: nanoid(), company: "amazon", position: "front -end" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev")); //morgan and .env
@@ -22,6 +39,8 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", messages: req.body }); //passing message
 });
+
+app.get("/api/v1");
 
 const port = process.env.PORT || 5100; //setting port using env
 
