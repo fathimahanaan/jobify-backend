85a539a73f35b9e51515e07cecb0bd4ec6f1836a
fathimahanaan <hanaanfh764@icloud.com>
2025-04-15 13:12:50 +0100

    server files updates

--- server.js
+++ server.js
@@ -31,16 +31,43 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json()); //middleware to accept data coming from front end
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+// app.post("/", (req, res) => {
+//   console.log(req);
+//   res.json({ message: "data received", messages: req.body }); //passing message
+// });
+
+// GET ALL JOBS
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "data received", messages: req.body }); //passing message
+//CREATE JOB
+app.get("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res
+      .status(400)
+      .json({ message: "please provide company and position" });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
 });
 
-app.get("/api/v1");
+//GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+});
 
 const port = process.env.PORT || 5100; //setting port using env
 
@@ -50,3 +77,4 @@ app.listen(port, () => {
 
 //whenever we using type:module don't forget to add .js when importing a file
 //hi this is a new command
+//hi this another commanf
